665a5440a1f20fe08d317b708d80df700a5cf5c2
Matteo Bettini <mb2389@cl.cam.ac.uk>
2022-11-21 14:05:06 +0000

    New eval for give_way

--- evaluate/format_videos.py
+++ evaluate/format_videos.py
@@ -1,13 +1,29 @@
 import os
 
-for subdir, dirs, files in os.walk(
-    "/Users/Matteo/SynologyDrive/Università/Cambridge/PhD/Papers/HetGPPO/videos"
-):
-    for file in files:
-        # print os.path.join(subdir, file)
-        filepath = subdir + os.sep + file
 
-        if filepath.endswith(".mp4"):
-            os.system(
-                f"ffmpeg -i {filepath} -vcodec libx265 -crf 28 {str(filepath).removesuffix('.mp4')}_compressed.mp4 "
-            )
+def compress_videos():
+    for subdir, dirs, files in os.walk(""):
+        for file in files:
+            # print os.path.join(subdir, file)
+            filepath = subdir + os.sep + file
+
+            if filepath.endswith(".mp4"):
+                os.system(
+                    f"ffmpeg -i {filepath} -vcodec libx265 -crf 28 {str(filepath).removesuffix('.mp4')}_compressed.mp4 "
+                )
+
+
+def mp4_to_gif():
+    for subdir, dirs, files in os.walk("/Users/Matteo/Downloads"):
+        for file in files:
+            # print os.path.join(subdir, file)
+            filepath = subdir + os.sep + file
+
+            if filepath.endswith(".mp4"):
+                os.system(
+                    f'ffmpeg -i {filepath} -vf "fps=30" -c:v pam -f image2pipe - | convert -delay 3 - -loop 0 -layers optimize {str(filepath).removesuffix(".mp4")}.gif'
+                )
+
+
+if __name__ == "__main__":
+    mp4_to_gif()
