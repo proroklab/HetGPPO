7ddec2b9c3690f1c9950b9e417f6ce1e47ee8690
Matteo Bettini <mb2389@cl.cam.ac.uk>
2023-04-13 15:10:26 +0200

    new multi_give way script

--- train/train_multi_give_way.py
+++ train/train_multi_give_way.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2022.
+#  Copyright (c) 2022-2023.
 #  ProrokLab (https://www.proroklab.org/)
 #  All rights reserved.
 
@@ -8,7 +8,6 @@ import pickle
 from ray import tune
 from ray.air.callbacks.wandb import WandbLoggerCallback
 from ray.rllib.algorithms.callbacks import MultiCallbacks
-from ray.rllib.models import MODEL_DEFAULTS
 
 from rllib_differentiable_comms.multi_trainer import MultiPPOTrainer
 from utils import PathUtils, TrainingUtils
@@ -16,8 +15,8 @@ from utils import PathUtils, TrainingUtils
 ON_MAC = False
 
 train_batch_size = 60000 if not ON_MAC else 200  # Jan 32768
-num_workers = 5 if not ON_MAC else 0  # jan 4
-num_envs_per_worker = 60 if not ON_MAC else 1  # Jan 32
+num_workers = 10 if not ON_MAC else 0  # jan 4
+num_envs_per_worker = 30 if not ON_MAC else 1  # Jan 32
 rollout_fragment_length = (
     train_batch_size
     if ON_MAC
@@ -36,18 +35,16 @@ def train(
     use_mlp,
     aggr,
     topology_type,
-    comm_range,
     add_agent_index,
     continuous_actions,
     seed,
     notes,
+    share_action_value,
+    comm_radius,
 ):
     checkpoint_rel_path = "ray_results/joint/HetGIPPO/MultiPPOTrainer_joint_654d9_00000_0_2022-08-23_17-26-52/checkpoint_001349/checkpoint-1349"
     checkpoint_path = PathUtils.scratch_dir / checkpoint_rel_path
     params_path = checkpoint_path.parent.parent / "params.pkl"
-
-    fcnet_model_config = MODEL_DEFAULTS.copy()
-    fcnet_model_config.update({"vf_share_layers": False})
 
     if centralised_critic and not use_mlp:
         if share_observations:
@@ -66,10 +63,15 @@ def train(
     if restore:
         with open(params_path, "rb") as f:
             config = pickle.load(f)
-
+    trainer = MultiPPOTrainer
+    trainer_name = "MultiPPOTrainer" if trainer is MultiPPOTrainer else "PPOTrainer"
     tune.run(
         MultiPPOTrainer,
         name=group_name if model_name.startswith("GPPO") else model_name,
+        checkpoint_freq=1,
+        keep_checkpoints_num=2,
+        checkpoint_at_end=True,
+        checkpoint_score_attr="episode_reward_mean",
         callbacks=[
             WandbLoggerCallback(
                 project=f"{scenario_name}{'_test' if ON_MAC else ''}",
@@ -79,7 +81,7 @@ def train(
             )
         ],
         local_dir=str(PathUtils.scratch_dir / "ray_results" / scenario_name),
-        stop={"training_iteration": 5000},
+        stop={"training_iteration": 1000},
         restore=str(checkpoint_path) if restore else None,
         config={
             "seed": seed,
@@ -91,11 +93,11 @@ def train(
             "clip_param": 0.2,  # 0.3
             "vf_loss_coeff": 1,  # Jan 0.001
             "vf_clip_param": float("inf"),
-            "entropy_coeff": 0.01,  # 0.01,
+            "entropy_coeff": 0,  # 0.01,
             "train_batch_size": train_batch_size,
             "rollout_fragment_length": rollout_fragment_length,
             "sgd_minibatch_size": 4096 if not ON_MAC else 100,  # jan 2048
-            "num_sgd_iter": 40,  # Jan 30
+            "num_sgd_iter": 45,  # Jan 30
             "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
             "num_workers": num_workers,
             "num_envs_per_worker": num_envs_per_worker,
@@ -103,30 +105,33 @@ def train(
             "gamma": 0.99,
             "use_gae": True,
             "use_critic": True,
+            "grad_clip": 40,
             "batch_mode": "complete_episodes",
             "model": {
+                "vf_share_layers": share_action_value,
                 "custom_model": model_name,
-                "custom_action_dist": "hom_multi_action",
+                "custom_action_dist": (
+                    "hom_multi_action" if trainer is MultiPPOTrainer else None
+                ),
                 "custom_model_config": {
-                    "activation_fn": "relu",
+                    "activation_fn": "tanh",
                     "share_observations": share_observations,
                     "gnn_type": "MatPosConv",
                     "centralised_critic": centralised_critic,
                     "heterogeneous": heterogeneous,
                     "use_beta": False,
                     "aggr": aggr,
-                    "topology_type": None,
-                    "comm_radius": comm_range,
+                    "topology_type": topology_type,
+                    "comm_radius": comm_radius,
                     "use_mlp": use_mlp,
                     "add_agent_index": add_agent_index,
                     "pos_start": 0,
                     "pos_dim": 2,
                     "vel_start": 2,
                     "vel_dim": 2,
-                    "share_action_value": True,
-                }
-                if model_name == "GPPO"
-                else fcnet_model_config,
+                    "share_action_value": share_action_value,
+                    "trainer": trainer_name,
+                },
             },
             "env_config": {
                 "device": "cpu",
@@ -140,13 +145,14 @@ def train(
                     "a_range": 1,
                     "obs_noise": 0,
                     "linear_friction": 0.1,
-                    "min_input_norm": 0.08,
+                    "min_input_norm": 0,
                     "box_agents": False,
                     "pos_shaping_factor": 1,
                     "final_reward": 0.005,
-                    "comm_range": comm_range,
+                    "comms_range": comm_radius,
                     "shared_rew": False,
                     "agent_collision_penalty": 0,
+                    "n_agents": 4,
                 },
             },
             "evaluation_interval": 20,
@@ -174,21 +180,22 @@ def train(
 if __name__ == "__main__":
     TrainingUtils.init_ray(scenario_name=scenario_name, local_mode=ON_MAC)
 
-    for seed in [3]:
+    for seed in [8]:
         train(
             seed=seed,
             restore=False,
             notes="",
             # Model important
             share_observations=True,
-            heterogeneous=False,
+            heterogeneous=True,
             # Other model
+            share_action_value=True,
             centralised_critic=False,
             use_mlp=False,
             add_agent_index=True,
             aggr="add",
-            topology_type=None,
-            comm_range=5,
+            topology_type="full",
+            comm_radius=-1,
             # Env
             max_episode_steps=200,
             continuous_actions=True,
